3f153470786a7dbd8f738c641ecd0d8a0ef94884
yunshengji <423450928@qq.com>
2018-09-07 22:35:40 +0800

    login works

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,26 @@
+class LoginController < ApplicationController
+URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+
+
+def wechat_params
+
+    { appid: "wx7b85d2d1429da2c6",
+    secret: "3be7b25d2eaf5454db4d44d7878e1571",
+    js_code: params[:code],
+    grant_type: "authorization_code" }
+
+end
+
+def wechat_user
+    @wechat_response ||= RestClient.post(URL, wechat_params)
+    @wechat_user ||=JSON.parse(@wechat_response.body)
+  end
+
+  def login
+    @user= User.find_or_create_by(open_id: wechat_user.fetch("openid"))
+
+    render json: {
+      userId: @user.id
+    }
+  end
+end
